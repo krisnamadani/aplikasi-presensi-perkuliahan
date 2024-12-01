5b761d0bbf8e4b31040937426758688c71f6c992
nabilabdullah <nabil.abdullah28@gmail.com>
2024-12-01 13:27:19 +0700

    Done feature login

---
+++ app/Http/Middleware/AuthenticateDosen.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthenticateDosen
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!Auth::guard('dosen')->check()) {
+            return redirect('/login'); // Atur sesuai route login Anda
+        }
+
+        return $next($request);
+    }
+}
