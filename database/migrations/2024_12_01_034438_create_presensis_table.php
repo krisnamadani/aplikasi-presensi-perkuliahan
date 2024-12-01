dfae22464a4597aad5d6b4460c539c911c4ff50c
Mohani <mohanisiskom12@gmail.com>
2024-12-01 10:56:47 +0700

    create migration and model

---
+++ database/migrations/2024_12_01_034438_create_presensis_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('presensis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('jadwal_id')->constrained('jadwals');
+            $table->foreignId('dosen_id')->constrained('dosens');
+            $table->timestamp('waktu_presensi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('presensis');
+    }
+};
